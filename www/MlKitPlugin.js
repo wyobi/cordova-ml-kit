b91b0a823eb37104a613e13d88265bec5ab8699d
Luis Bouça <luismrbouca@gmail.com>
2019-11-21 14:53:43 +0000

    Configuration Bug Fix, Configure Label identification
    fixed a bug in configuration
    configured label pathing
    configured label necessary imports
    bumped some mlvision versions

--- www/MlKitPlugin.js
+++ www/MlKitPlugin.js
@@ -6,3 +6,9 @@ exports.getText = function (success, error,takePicture,cloud,lang) {
 
     exec(success, error, 'MlKitPlugin', 'getText', args);
 };
+exports.getLabel = function (success, error,takePicture,cloud) {
+
+    var args = [takePicture,cloud];
+
+    exec(success, error, 'MlKitPlugin', 'getLabel', args);
+};
